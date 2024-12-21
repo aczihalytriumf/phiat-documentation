f635f6ce77274c21f33a4cca9ffb0f2aa3defdf7
aczihalytriumf <154386361+aczihalytriumf@users.noreply.github.com>
2024-12-21 12:40:48 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PhIAT'
+copyright = '2024, TITAN Collaboration'
+author = 'TITAN Collaboration'
 
 release = '0.1'
 version = '0.1.0'
